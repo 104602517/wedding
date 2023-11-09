f0a9e6056edc63c95bf4ebd439ecdc9c8310084e
Allennia <allennia@seektopser.com>
2023-11-09 13:55:18 +0800

    Fix , .

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -69,7 +69,7 @@ const AboutUsIndex: React.FC  = ()=> {
         play
         speed={1}
         segments={[1, 125]}
-        animationData={jumpRabbit}/>,
+        animationData={jumpRabbit}/>
       <div className="groom">
         <span className="avatar"></span>
         <p className="intro">個性孤僻，喜好人煙稀少的環境，除了健身房以外，不願意出現在任何人口稠密區。一天的行程固定是起床看股票寫程式吃飯運動睡覺 (通常只有吃飯跟運動不是躺著)，如果前一天晚上沒有把眼鏡放好，會增加十分鐘的眼鏡摸索行程。最喜歡的休閒活動是霸凌小狗(特別是黑狗)，如果時間允許，一週會安排擼狗一次。胸無大志而且懶惰，卻有無可救藥的天真理想，期望有一天能完全放下鍵盤，遠離一切世俗紛爭，成為都會隱士。目前為一名全遠端的軟體工程師，生活一成不變地美好，為此網站的編程/架設者。</p>
